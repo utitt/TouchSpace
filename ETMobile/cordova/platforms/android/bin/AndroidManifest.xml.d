65826265874cac158ac1ab2fa5e08d81f5630035
GG <gerald_721@hotmail.com>
2013-10-12 16:32:01 +0800

    clear build file

--- ETMobile/cordova/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/User/workspace/TouchSpace/ETMobile/cordova/platforms/android/bin/AndroidManifest.xml : \
-/Users/User/workspace/TouchSpace/ETMobile/cordova/platforms/android/AndroidManifest.xml \
--- ETMobile/cordova/platforms/android/bin/ETMobile-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/User/workspace/TouchSpace/ETMobile/cordova/platforms/android/bin/ETMobile-debug-unaligned.apk : \
-/Users/User/workspace/TouchSpace/ETMobile/cordova/platforms/android/bin/ETMobile.ap_ \
-/Users/User/workspace/TouchSpace/ETMobile/cordova/platforms/android/bin/classes.dex \
-/Users/User/workspace/TouchSpace/ETMobile/cordova/platforms/android/libs/cordova-3.1.0.jar \
-/Users/User/workspace/TouchSpace/ETMobile/cordova/platforms/android/libs/com.google.zxing.client.android.captureactivity.jar \
